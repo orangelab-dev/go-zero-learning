8614d5ad413e87908319ef017e34ceca7b132cf6
orange <orangelgj0228@gmail.com>
2026-04-26 14:33:30 +0800

    feat: 建立了个最简单的 api 示例

---
+++ examples/greet/internal/logic/greetlogic.go
@@ -0,0 +1,35 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.10.1
+
+package logic
+
+import (
+	"context"
+
+	"greet/internal/svc"
+	"greet/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GreetLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGreetLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GreetLogic {
+	return &GreetLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GreetLogic) Greet(req *types.Request) (resp *types.Response, err error) {
+	// todo: add your logic here and delete this line
+
+	return &types.Response{
+		Message: "你好" + req.Name,
+	}, nil
+}
